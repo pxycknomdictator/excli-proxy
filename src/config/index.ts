f39ffb92e323d1e3760a6854eaf2533d41a82259
Noman <pxycknomdictator@outlook.com>
2026-04-02 22:22:33 +0500

    update: web server emojis

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 import type { INTERACTIVE_PROMPTS } from "../types";
 
 export const WEB_SERVERS: INTERACTIVE_PROMPTS[] = [
-    { label: "Nginx", emoji: "🔥", value: "nginx" },
-    { label: "Caddy", emoji: "⚡", value: "caddy" },
+    { label: "Nginx", emoji: "🏎️", value: "nginx" },
+    { label: "Caddy", emoji: "🪄", value: "caddy" },
 ];
