c381dab7123e68d7a7350e26182e7643e2c32e89
Noman <pxycknomdictator@outlook.com>
2026-04-04 18:34:35 +0500

    update: change emoji

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import type { INTERACTIVE_PROMPTS } from "../types";
 export const BANNER_FONT = "Standard";
 
 export const WEB_SERVERS: INTERACTIVE_PROMPTS[] = [
-    { label: "Nginx", emoji: "🏎️", value: "nginx" },
+    { label: "Nginx", emoji: "⚡", value: "nginx" },
     { label: "Caddy", emoji: "🪄", value: "caddy" },
 ];
 
