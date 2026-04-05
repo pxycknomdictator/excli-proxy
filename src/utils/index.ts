e82214f58d4a841fa9bddbd3f0ffc947821c5b62
Noman <pxycknomdictator@outlook.com>
2026-04-06 03:10:03 +0500

    feat: add utility fn to load and dum yaml file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,42 @@
+import yaml from "js-yaml";
 import { existsSync } from "node:fs";
-import { cp, readFile } from "node:fs/promises";
+import { cp, readFile, writeFile } from "node:fs/promises";
 import { cancel } from "@clack/prompts";
-import type { COPY_FN_PROPS, INTERACTIVE_PROMPTS } from "../types";
+import type {
+    COPY_FN_PROPS,
+    DockerComposeConfig,
+    GenerateFileArgs,
+    INTERACTIVE_PROMPTS,
+} from "../types";
 
 export async function copy({ copyPath, targetDir }: COPY_FN_PROPS) {
     try {
         await cp(copyPath, targetDir, { recursive: true });
     } catch (error) {
         throw new Error(`Failed to copy: ${error}`);
+    }
+}
+
+export async function loadYaml(location: string): Promise<DockerComposeConfig> {
+    try {
+        const yamlContent = await read(location);
+        const objectContent = yaml.load(yamlContent) as DockerComposeConfig;
+        return objectContent;
+    } catch (error) {
+        throw new Error(`failed to read yaml file: ${error}`);
+    }
+}
+
+export function dumpYaml(content: DockerComposeConfig): string {
+    const yamlContent = yaml.dump(content, { indent: 4 });
+    return yamlContent;
+}
+
+export async function write({ fileContent, fileLocation }: GenerateFileArgs) {
+    try {
+        await writeFile(fileLocation, fileContent, "utf-8");
+    } catch (error) {
+        throw new Error(`Failed to write file: ${error}`);
     }
 }
 
