c3ef2cdea5fb3ebc59305fa2d399e689fba78ea9
Noman <pxycknomdictator@outlook.com>
2026-04-09 16:01:13 +0500

    feat: add caddy docker configs

--- src/proxy/caddy.ts
+++ src/proxy/caddy.ts
@@ -1,5 +1,48 @@
-import type { Config } from "../types";
+import type { Config, DockerComposeConfig, WEB_SERVER_MODE } from "../types";
+
+function generateCaddyDockerComposeYaml(mode: WEB_SERVER_MODE) {
+    const caddy: DockerComposeConfig = {
+        services: {
+            caddy: {
+                image: "caddy:2.11.2",
+                container_name: "caddy",
+                ports: ["80:80"],
+                volumes: ["./caddy/Caddyfile:/etc/caddy/Caddyfile:ro"],
+                depends_on: ["server", "database"],
+                networks: ["app_network"],
+            },
+        },
+    };
+
+    const server: DockerComposeConfig = {
+        services: {
+            server: {
+                build: {
+                    context: ".",
+                    dockerfile: "Dockerfile",
+                },
+                env_file: [".env"],
+                networks: ["app_network"],
+                depends_on: ["database"],
+            },
+        },
+    };
+
+    if (mode === "reverse_proxy") {
+        server.services.server!.container_name = "server";
+    }
+
+    const merged: DockerComposeConfig = {
+        services: {
+            ...caddy.services,
+            ...server.services,
+        },
+    };
+
+    return merged;
+}
 
 export async function setupCaddy(config: Config) {
+    generateCaddyDockerComposeYaml(config.webServerMode);
     console.log("Caddy", config);
 }
