40fc9d373daa745d9aa7315318b6559f2d89b2d2
Noman <pxycknomdictator@outlook.com>
2026-04-17 23:33:31 +0500

    feat: add traefik prompt option in cli

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,6 +16,7 @@ export const BANNER_FONT = "Standard";
 export const WEB_SERVERS: INTERACTIVE_PROMPTS[] = [
     { label: "Nginx", emoji: "⚡", value: "nginx" },
     { label: "Caddy", emoji: "🪄", value: "caddy" },
+    { label: "Traefik", emoji: "🛣️", value: "traefik" },
 ];
 
 export const USES_OF_WEB_SERVER: INTERACTIVE_PROMPTS[] = [
